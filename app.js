0f6fc9cb070a6d34b40e30945ffab670a65e55cf
omara <omarabdelal69@gmail.com>
2023-01-17 19:05:35 +0200

    commit

--- app.js
+++ app.js
@@ -2,21 +2,23 @@ const createError = require('http-errors');
 const path = require('path');
 const logger = require('morgan');
 const express = require('express');
-const postman = require('postman-api-client');
+//const postman = require('postman-api-client');
 
-
-
+const indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/', indexRouter);
+app.use('/api', apiRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
---
+++ routes/api.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+const db = require('../models'); //contain the models
+//delete all the saved images----------------------------------------------------------------
+
+
+module.exports = router;
